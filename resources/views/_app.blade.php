5926865aa1be982343ff89cddfceebeb89b75c7a
hodev-dev <hodev.dev@gmail.com>
2021-08-05 18:37:37 +0100

    google bot

--- resources/views/_app.blade.php
+++ resources/views/_app.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="google-site-verification" content="q9OXpUOG_7_UUa-HIzGlrFoPwKkiyQ63hKDr8pkQI_Y" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{json_decode($config)->title}}</title>
 </head>
